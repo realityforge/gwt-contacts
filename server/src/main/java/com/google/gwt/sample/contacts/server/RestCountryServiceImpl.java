10b6d7ac25a7c5ae184a1012e613a3949d2e82f5
Peter Donald <peter@realityforge.org>
2011-08-05 13:47:03 +1000

    WIP for servlet testing

--- server/src/main/java/com/google/gwt/sample/contacts/server/RestCountryServiceImpl.java
+++ server/src/main/java/com/google/gwt/sample/contacts/server/RestCountryServiceImpl.java
@@ -1,37 +1,37 @@
 package com.google.gwt.sample.contacts.server;
 
-import com.google.gwt.sample.contacts.shared.Contact;
-import com.google.gwt.sample.contacts.shared.ContactDetails;
-import com.google.gwt.sample.contacts.shared.ContactsService;
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-import java.util.ArrayList;
-import javax.ejb.EJB;
+import java.io.IOException;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings( "serial" )
+@WebServlet( urlPatterns = { "/ds/xml/responses/country_fetch_rest.xml" } )
 public class RestCountryServiceImpl
-  extends RemoteServiceServlet
-  implements ContactsService
+  extends HttpServlet
 {
-  @EJB
-  private LocalContacts _contacts;
-
-  public Contact createOrUpdateContact( final Contact contact )
+  @Override
+  protected void doGet( final HttpServletRequest req, final HttpServletResponse resp )
+    throws ServletException, IOException
   {
-    return _contacts.createOrUpdateContact( contact );
-  }
-
-  public void deleteContacts( final ArrayList<String> ids )
-  {
-    _contacts.deleteContacts( ids );
-  }
-
-  public ArrayList<ContactDetails> getContactDetails()
-  {
-    return _contacts.getContactDetails();
-  }
-
-  public Contact getContact( final String id )
-  {
-    return _contacts.getContact( id );
+    System.out.println( "RestCountryServiceImpl.doGet" );
+    final Enumeration headers = req.getHeaderNames();
+    while ( headers.hasMoreElements() )
+    {
+      final String s = (String) headers.nextElement();
+      System.out.println( s + " = " + req.getHeader( s ) );
+    }
+    final ServletInputStream inputStream = req.getInputStream();
+    final StringBuilder sb = new StringBuilder();
+    while ( inputStream.available() > 0 )
+    {
+      sb.append( (char) inputStream.read() );
+    }
+    System.out.println( sb );
+    super.doGet( req, resp );
   }
 }
