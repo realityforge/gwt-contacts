c1efb60c5a50ad724aa735767f102d8cc5a70212
Peter Donald <peter@realityforge.org>
2011-08-30 17:25:26 +1000

    Log messages a problem occurs during startup

--- client/src/main/java/com/google/gwt/sample/contacts/client/Contacts.java
+++ client/src/main/java/com/google/gwt/sample/contacts/client/Contacts.java
@@ -1,5 +1,6 @@
 package com.google.gwt.sample.contacts.client;
 
+import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.sample.contacts.client.gin.ContactGinjector;
 import com.google.gwt.sample.contacts.client.gin.ContactsServicesGinModule;
@@ -11,10 +12,14 @@ import com.google.gwt.user.client.rpc.XsrfToken;
 import com.google.gwt.user.client.rpc.XsrfTokenService;
 import com.google.gwt.user.client.rpc.XsrfTokenServiceAsync;
 import com.google.gwt.user.client.ui.RootPanel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public final class Contacts
-  implements com.google.gwt.core.client.EntryPoint
+    implements EntryPoint
 {
+  private static final Logger LOG = Logger.getLogger( "EntryPoint" );
+
   public void onModuleLoad()
   {
     final XsrfTokenServiceAsync xsrf = (XsrfTokenServiceAsync) GWT.create( XsrfTokenService.class );
@@ -30,17 +35,17 @@ public final class Contacts
         {
           throw caught;
         }
-        catch ( final RpcTokenException e )
+        catch( final RpcTokenException e )
         {
           // Can be thrown for several reasons:
-          //   - duplicate session cookie, which may be a sign of a cookie
-          //     overwrite attack
-          //   - XSRF token cannot be generated because session cookie isn't
-          //     present
+          //   - duplicate session cookie, which may be a sign of a cookie overwrite attack
+          //   - XSRF token cannot be generated because session cookie isn't present
+          LOG.log( Level.SEVERE, "Problem generating RPC token: Possible causes: duplicate session cookie which may be a sign of a cookie overwrite attack or XSRF token cannot be generated because session cookie isn't present", e  );
         }
-        catch ( final Throwable e )
+        catch( final Throwable e )
         {
           // unexpected
+          LOG.log( Level.SEVERE, "Unexpected problem generating security token", e  );
         }
         Window.alert( "Error generating security token. Please reload page." );
       }
@@ -55,6 +60,15 @@ public final class Contacts
 
   private void startupApplication()
   {
+    /* Add handlers, setup activities */
+    GWT.setUncaughtExceptionHandler( new GWT.UncaughtExceptionHandler()
+    {
+      public void onUncaughtException( final Throwable e )
+      {
+        LOG.log( Level.SEVERE, "Unexpected problem with application", e  );
+      }
+    } );
+
     final ContactGinjector injector = GWT.create( ContactGinjector.class );
 
     // Force the creation of the ActivityManager
