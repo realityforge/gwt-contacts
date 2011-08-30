125ad842dba8b6642f5e737cc98f146ebeb3b746
Peter Donald <peter@realityforge.org>
2011-08-31 09:47:18 +1000

    Turn DesktopSHell into a UI bound component

--- client/src/main/java/com/google/gwt/sample/contacts/client/view/ui/DesktopShellUI.java
+++ client/src/main/java/com/google/gwt/sample/contacts/client/view/ui/DesktopShellUI.java
@@ -1,22 +1,38 @@
 package com.google.gwt.sample.contacts.client.view.ui;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.sample.contacts.client.view.DesktopShellView;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
+import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class DesktopShellUI
+  extends Composite
   implements DesktopShellView
 {
-  private final SimplePanel _display = new SimplePanel();
+  interface Binder extends UiBinder<Widget, DesktopShellUI> {}
+
+  private static final Binder uiBinder = GWT.create( Binder.class );
+  @UiField
+  SimplePanel _detailsPanel;
+  @UiField
+  SimplePanel _masterPanel;
+
+  public DesktopShellUI()
+  {
+    initWidget( uiBinder.createAndBindUi( this ) );
+  }
 
   public AcceptsOneWidget getMasterRegion()
   {
-    return _display;
+    return _masterPanel;
   }
 
   public Widget asWidget()
   {
-    return _display;
+    return this;
   }
 }
