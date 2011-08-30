4a1568a611887fabb18b554398f94011634a1b0b
Peter Donald <peter@realityforge.org>
2011-08-30 19:30:09 +1000

    Optimise imports

--- client/src/main/java/com/google/gwt/sample/contacts/client/gin/ApplicationInjector.java
+++ client/src/main/java/com/google/gwt/sample/contacts/client/gin/ApplicationInjector.java
@@ -1,6 +1,5 @@
 package com.google.gwt.sample.contacts.client.gin;
 
-import com.google.gwt.inject.client.Ginjector;
 import com.google.gwt.sample.contacts.client.view.ApplicationShell;
 
 public interface ApplicationInjector
