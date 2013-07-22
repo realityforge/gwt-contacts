92856a3197531955a7c71fb04b45847f164fb481
Peter Donald <peter@realityforge.org>
2013-07-22 21:38:57 +1000

    Match the database name

--- config/setup.sh
+++ config/setup.sh
@@ -9,7 +9,7 @@ asadmin create-jdbc-connection-pool\
   --isconnectvalidatereq=true\
   --validationmethod auto-commit\
   --ping true\
-  --property "ServerName=127.0.0.1:User=${USER}:Password=letmein:PortNumber=5432:DatabaseName=${USER}_Contacts_DEV" ContactsPool
+  --property "ServerName=127.0.0.1:User=${USER}:Password=letmein:PortNumber=5432:DatabaseName=${USER}_CONTACTS_DEV" ContactsPool
 asadmin create-jdbc-resource --connectionpoolid ContactsPool jdbc/ContactsDS
 
 asadmin set domain.resources.jdbc-connection-pool.ContactsPool.property.JDBC30DataSource=true
