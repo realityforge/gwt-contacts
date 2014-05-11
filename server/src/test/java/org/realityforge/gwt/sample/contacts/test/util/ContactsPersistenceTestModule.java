6688d0133d38516e2f97f4c17d1cac1a081b77ba
Peter Donald <peter@realityforge.org>
2014-05-11 17:49:42 +1000

    Correctly quote table for postgres

--- server/src/test/java/org/realityforge/gwt/sample/contacts/test/util/ContactsPersistenceTestModule.java
+++ server/src/test/java/org/realityforge/gwt/sample/contacts/test/util/ContactsPersistenceTestModule.java
@@ -3,6 +3,8 @@ package org.realityforge.gwt.sample.contacts.test.util;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import javax.annotation.Nonnull;
+import javax.persistence.Table;
 import org.postgresql.Driver;
 import org.realityforge.guiceyloops.server.PersistenceTestModule;
 import org.realityforge.gwt.sample.contacts.server.entity.ContactsPersistenceUnit;
@@ -34,5 +36,13 @@ public class ContactsPersistenceTestModule
     Collections.reverse( tables );
 
     requestCleaningOfTables( tables.toArray( new String[ tables.size() ] ) );
+  }
+
+  @Nonnull
+  protected final String toQualifiedTableName( final Table annotation )
+  {
+    final String tableName = annotation.name();
+    //Correctly quote table for postgres
+    return "\"" + annotation.schema() + "\".\"" + ( tableName.startsWith( "vw" ) ? tableName.replace( "vw", "tbl" ) : tableName ) + "\"";
   }
 }
